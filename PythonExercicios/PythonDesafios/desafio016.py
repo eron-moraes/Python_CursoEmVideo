4f23622168d7076ea3e139a3a203efc03dd5e183
Eron Moraes <eron.moraes7@gmail.com>
2023-06-22 23:38:02 -0300

    praticando

---
+++ PythonExercicios/PythonDesafios/desafio016.py
@@ -0,0 +1,8 @@
+'''from math import trunc
+
+num = float(input("Digite um número quebrado: "))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))'''
+
+num = float(input("Digite um número quebrado: "))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
+
--- PythonExercicios/PythonTest/aula08a.py
+++ PythonExercicios/PythonTest/aula08a.py
@@ -1,4 +1,4 @@
 import math
 num = int(input('Digite um numero: '))
 raiz = math.sqrt(num)
-print('A raiz de {} é igual a {}'.format(num, raiz))
+print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
